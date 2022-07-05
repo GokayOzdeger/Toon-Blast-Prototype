bd89d2cc985255f4eb3a8823145a32b2c7d8892d
Gökay <ygokayo@hotmail.com>
2022-07-05 16:54:41 +0300

    scene cleared

--- Assets/_Scripts/Level/LevelConfig.cs
+++ Assets/_Scripts/Level/LevelConfig.cs
@@ -5,8 +5,5 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "ScriptableObjects/Level Config")]
 public class LevelConfig : ScriptableObject
 {
-    [Group]
-    [SerializeField] private GridController.GridControllerSettings gridControllerSettings;
 
-    public GridController.GridControllerSettings GridControllerSettings => gridControllerSettings;
 }
--- Assets/_Scripts/Level/LevelController.cs
+++ Assets/_Scripts/Level/LevelController.cs
@@ -5,21 +5,15 @@ using UnityEngine;
 public class LevelController
 {
     private LevelConfig _config;
-
-    private GridController _gridController;
     
     public LevelController(LevelConfig config, LevelSceneReferences levelSceneReferences)
     {
         this._config = config;
-
-        _gridController = new GridController(_config.GridControllerSettings, levelSceneReferences.GridControllerSceneReferences);
     }
 
     [System.Serializable]
     public class LevelSceneReferences
     {
-        [Group]
-        [SerializeField] private GridController.GridControllerSceneReferences gridControllerSceneReferences;
-        public GridController.GridControllerSceneReferences GridControllerSceneReferences => gridControllerSceneReferences;
+
     }
 }
